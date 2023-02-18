93acf218c5c95fd1a6c353dd10ddecc1f05dcf32
Gregorino Al Josan <68318284+rinogrego@users.noreply.github.com>
2023-02-18 19:35:45 +0700

    add "start page until end page" feature

--- download_image.py
+++ download_image.py
@@ -9,6 +9,8 @@ import os
 parser = argparse.ArgumentParser(description='Download Image from Website/Storage Link')
 parser.add_argument('url', type=str, help='Specify link/url where the image is stored')
 parser.add_argument('--p', type=int, help='Number of images if multiple images exist')
+parser.add_argument('--ps', type=int, help='Page start')
+parser.add_argument('--pe', type=int, help='Page end')
 parser.add_argument('--ifor', type=str, help='Image format (jpg/png/jpeg/etc). Default: jpg')
 parser.add_argument('--tf', type=str, help='Specify folder in which the downloaded image will be saved to. Default: current working directory')
 
@@ -16,6 +18,8 @@ args = parser.parse_args()
 
 main_image_url = args.url
 pages = args.p
+page_start = args.ps
+page_end = args.pe
 image_format = args.ifor
 target_folder = args.tf
 
@@ -23,25 +27,32 @@ target_folder = args.tf
 # Set default image format to jpg
 if args.ifor == None:
     image_format = 'jpg'
+ 
+    
+# Set pages if not specified and the task is downloading multiple images
+if args.p == None and main_image_url[-1] == "/":
+    pages = page_end - page_start
 
 
 # Set workdir if the target folder is specified to save the images
 if target_folder is not None:
     if not os.path.isdir(target_folder):
-        os.makedirs(target_folder)
-    os.chdir(target_folder)
+        os.mkdir(target_folder)
+    if os.getcwd() != target_folder:
+        os.chdir(target_folder)
     print("Working Directory:", target_folder)
 
 
 # Function to download image/images
 def download_image(pages_range):
     if pages_range is not None:
+        assert type(pages_range) is list
         for page in pages_range:
             image_url = main_image_url+f'{page}.{image_format}'
             print('Downloading image from: ', image_url)
             img_data = requests.get(image_url).content
 
-            with open(f'{page}.{image_format}', 'wb') as handler:
+            with open(f'{page}'.zfill(3)+f'.{image_format}', 'wb') as handler:
                 handler.write(img_data)
     else:
         # if the image is just one and then the image format is specified from the url
@@ -54,11 +65,19 @@ def download_image(pages_range):
 
 
 if __name__ == '__main__':
+    print("Test", multiprocessing.current_process())
     if pages is not None:
         # If the number of pages is more than one then distribute the process
         # Multiprocessing things
         cpus = multiprocessing.cpu_count()
-        pages_list = [page for page in range(0, pages+1)]
+        if page_start != None:
+            assert page_start > 0
+            if page_end != None:
+                pages_list = [page for page in range(page_start-1, page_end+1)]
+            else:
+                pages_list = [page for page in range(page_start-1, pages+1)]
+        else:
+            pages_list = [page for page in range(0, pages+1)]
         pages_per_chunks = math.ceil(pages/cpus)
         pages_chunks = [pages_list[x:x+pages_per_chunks] for x in range(1, pages+1, pages_per_chunks)]
 
